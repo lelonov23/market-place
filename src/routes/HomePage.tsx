ef9fd7cef8a0d13043529f6f44f51a0a58dbfc45
Stepan Leonov <stepan.leonov777@gmail.com>
2022-04-05 14:02:06 +0300

    test select from redux

--- src/routes/HomePage.tsx
+++ src/routes/HomePage.tsx
@@ -1,9 +1,22 @@
 import React from "react";
 
+import { useAppSelector } from "../app/hooks";
+import { selectWarehouse } from "../warehouseSlice";
+
 function HomePage() {
+  const categories = useAppSelector(selectWarehouse);
   return (
     <div>
       <h1>Home Page</h1>
+      <ul>
+        {categories.map((cat) => {
+          return (
+            <li>
+              <h2>{cat.name}</h2>
+            </li>
+          );
+        })}
+      </ul>
     </div>
   );
 }
