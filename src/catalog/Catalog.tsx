575e25c7882d6df6b9b8aea4b0f49d9551bcdc9c
Stepan Leonov <stepan.leonov777@gmail.com>
2022-04-11 16:37:28 +0300

    add styling to catalog and homepage

--- src/catalog/Catalog.tsx
+++ src/catalog/Catalog.tsx
@@ -17,35 +17,65 @@ const Catalog: React.FC = () => {
 
   return (
     <div className={styles.tabs}>
-      <ul className={styles.cats}>
-        {Store.mainCategories.map((category) => {
-          return (
-            <li
-              key={category.id}
-              onClick={() => handleOpenCategory(category.id)}
-              className={activeCategory === category.id ? styles.active : ""}
-            >
-              {category.name}
-            </li>
-          );
-        })}
-      </ul>
-      <div className="outlet">
-        {Store.mainCategories.map((category) => {
-          return (
-            <CategoryContent activeCategory={activeCategory} id={category.id}>
-              {Store.subcategories
-                .filter((subcat) => subcat.categoryId === category?.id)
-                .map((subcat) => {
-                  return (
-                    <div key={subcat.id}>
-                      <p>{subcat.name}</p>
-                    </div>
-                  );
-                })}
-            </CategoryContent>
-          );
-        })}
+      <div className={styles.catList}>
+        <h2>Каталог</h2>
+        <ul className={styles.cats}>
+          {Store.mainCategories.map((category) => {
+            return (
+              <li
+                key={category.id}
+                onClick={() => handleOpenCategory(category.id)}
+                className={
+                  activeCategory === category.id
+                    ? `${styles.active} ${styles.categoryLink}`
+                    : styles.categoryLink
+                }
+              >
+                <p>{category.name}</p>
+              </li>
+            );
+          })}
+        </ul>
+      </div>
+
+      <div className={styles.subcatList}>
+        <h2>
+          {
+            Store.mainCategories.find(
+              (category) => category.id === activeCategory
+            )?.name
+          }
+        </h2>
+        <ul>
+          {Store.mainCategories.map((category) => {
+            return (
+              <CategoryContent activeCategory={activeCategory} id={category.id}>
+                <ul className={styles.subcats}>
+                  {Store.subcategories
+                    .filter((subcat) => subcat.categoryId === category?.id)
+                    .map((subcat) => {
+                      return (
+                        <li key={subcat.id}>
+                          <p className={styles.subcatHeading}>{subcat.name}</p>
+                          <ul>
+                            {Store.subcategories
+                              .filter((cat) => cat.categoryId === subcat.id)
+                              .map((cat) => {
+                                return (
+                                  <li>
+                                    <p>{cat.name}</p>
+                                  </li>
+                                );
+                              })}
+                          </ul>
+                        </li>
+                      );
+                    })}
+                </ul>
+              </CategoryContent>
+            );
+          })}
+        </ul>
       </div>
     </div>
   );
